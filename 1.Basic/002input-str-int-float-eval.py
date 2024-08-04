b1049db1588bf188d6114ed3265d9dcf5fc7ce96
rosi329 <rosi329atk@gmail.com>
2024-08-04 21:28:59 +0500

    002input-str-int-float-eval.py
    input value as sum of strings integer float eval

---
+++ 1.Basic/002input-str-int-float-eval.py
@@ -0,0 +1,41 @@
+#   input name
+
+name = input("what is your name: ")
+print("Mr.",name) # Mr.rosi329
+
+#   quote
+
+print("Mr.'rosi329' deploy python scripts.")
+print('Mr."rosi329" deploy python scripts.')
+
+
+
+# name = input("what is name: ")
+# print ("Mr.",name)
+
+
+#   input string
+
+a = input("enter value for a: ") # 33
+b = input("enter value for b: ") # 22
+c= a+b 
+print("The sum of string a & b: ",c) # 3322
+
+#   input integer
+
+d = int(input("enter value for d: ")) # 76
+e = int(input("enter value for e: ")) # 59
+f= d+e
+print("The sum of integer d & e: ",f) # 135
+
+#   input float
+g = float(input("enter for value g: ")) #   123 / 123.0
+h = float(input("enter for value h: ")) #   43 /  43.0
+i = g+h 
+print("The sum of float num g & h: ",i) #   166.0
+
+#   input eval
+j = eval(input("enter value for  j: ")) # 75
+k = eval(input("enter value for  k: ")) # 27
+l = j+k
+print("The sum of eval j & k: ",l)      #    102
