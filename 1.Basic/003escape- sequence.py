288ae7801d9949e611886da34162339c7cc31467
rosi329 <rosi329atk@gmail.com>
2024-08-09 20:27:23 +0500

    003escape- sequence.py

---
+++ 1.Basic/003escape- sequence.py
@@ -0,0 +1,24 @@
+# escape sequence
+
+# normal line sequence
+print("The quick brown fox jumps over the lazy dog.") # The quick brown fox jumps over the lazy dog.
+
+# 'single quote line sequence'
+print("The quote:'He is rosi329'") # The quote:'He is rosi329'
+
+# "Double quoted line sequence"
+print("The quote:\"rosi329 is here!!!\"") # The quote:"rosi329 is here!!!"
+
+# \n new line sequence
+print("Greeting friends. \nHow are you. \nHow the day is?") 
+# Greeting friends.
+# How are you. 
+# How the day is?
+
+# \t tab line sequence
+print("The quickly represntation\t deploy by \t jr.faculty")   
+#  The quickly represntation        deploy by       jr.faculty 
+
+# \\ backslash line sequence
+print("Differnce b\\w linux & windows ")
+# Difference b\w linux & windows
