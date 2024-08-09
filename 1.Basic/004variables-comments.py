d8c9792560e50b8f53434eb8fecc8a4521b8a85b
rosi329 <rosi329atk@gmail.com>
2024-08-09 20:27:57 +0500

    004variables-comments.py

---
+++ 1.Basic/004variables-comments.py
@@ -0,0 +1,34 @@
+# varibles & commnets
+
+# Valid varibles 
+myvar = 1
+myVar = 2
+MyVar = 3
+MYVAR = 4
+_1myvar = 5
+_myvar1 = 6
+
+print(myvar) # 1
+print(myVar) # 2
+print(MyVar) # 3
+print(MYVAR) # 4
+print(_1myvar) # 5
+print(_myvar1) # 6
+
+
+# InValid varibles
+# 1mywar = 7
+# my var = 8
+# $myvar = 9
+# my-var = 10
+
+
+# comments
+
+# single line commnet
+
+"""
+multi 
+line 
+comment 
+"""
